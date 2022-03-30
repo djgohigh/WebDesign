ef787471958c90bdbf70181a0e270bc80d5d0bbf
selfMade <88639900+peaks26@users.noreply.github.com>
2022-03-30 15:06:18 +0900

    Update script.js

--- Corona-Live/script.js
+++ Corona-Live/script.js
@@ -1,10 +1,14 @@
 let getData = [];
 
 $(function () {
+    $.ajaxPrefilter('json', fucntion(options, orig, jqXHR) {
+        return 'jsonp';
+    });
     $.ajax({
         type: "GET",
+        crossDomain: true,
         url: "http://openapi.seoul.go.kr:8088/6758584479646a38313033687442516b/json/TbCorona19CountStatus/1/5/",
-        data: "jsonp",
+        data: json,
         async: false,
         success: function (response) {
             getData = response;
